f1895ab5f984c233f0206c484f5ebb8b1f021112
Cassie Huang <CassieHuang@MacBook-Pro.local>
2020-12-06 15:49:26 -0500

    word processor algorithm

--- prototype/ebook-app-server/server.js
+++ prototype/ebook-app-server/server.js
@@ -158,11 +158,63 @@ app.get('/songs/:token/:playlistid', async (req, res) => {
 //helper funtion for text processing and database lookup
 //the parameter "text" is a string of all the lyrics combined
 const processLyrics = async (text) => {
-    let result = [];
-  
-    result = [{title:"Harry Potter", author:"J.K. Rowling", eBookUrl: "www.google.com"},
+    let words = text.trim().split(" ");
+
+    //themes to choose from (destined to change)
+    const romance = ["love", "like", "cute", "adorable", "baby", "babe", "amazing", 
+                    "beautiful", "joy", "loving", "heart", "couple", "romance"];
+    const horror = ["kill", "curse", "die", "dead"];
+    const angst = ["hate", "anxious", "anxiety", "worry", "wreath", "helpless"];
+    const adventure = ["run", "fly", "get away", "challenge", "venture", "journey", "vacation", "thrill"];
+    const youngAdult = ["relationship", "life", "hope", "boy", "girl", "couple", "growth", "young", "adult","youth"];
+    const fantasy = ["demon", "magic", "sky", "ship", "explore", "dream", "fire", 
+                    "dragon", "earth", "universe"];
+
+    let romanceCounter = 0;
+    let horrorCounter = 0;
+    let angstCounter = 0;
+    let adventureCounter = 0;
+    let youngAdultCounter = 0;
+    let fantasyCounter = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        if (romance.includes(words[i])) {
+            romanceCounter += 1;
+        } else if (horror.includes(words[i])) {
+            horrorCounter += 1;
+        } else if (angst.includes(words[i])) {
+            angstCounter += 1;
+        } else if (adventure.includes(words[i])) {
+            adventureCounter += 1;
+        } else if (youngAdult.includes(words[i])) {
+            youngAdultCounter += 1;
+        } else if (fantasy.includes(words[i])) {
+            fantasyCounter += 1;
+        }
+    }
+
+    let result = "";
+    const maxCount = Math.max(romanceCounter, horrorCounter, angstCounter, adventureCounter,youngAdultCounter,fantasyCounter);
+    if (maxCount === romanceCounter) {
+        result = "romance";
+    } else if (maxCount === horrorCounter) {
+        result = "horror";
+    } else if (maxCount === angstCounter) {
+        result = "angst";
+    } else if (maxCount === adventureCounter) {
+        result = "adventure";
+    } else if (maxCount === youngAdultCounter) {
+        result = "young adult";
+    } else if (maxCount === fantasyCounter) {
+        result = "fantasy";
+    } else {
+        result = "not found";
+    }
+    
+    //console.log(result);
+    /*result = [{title:"Harry Potter", author:"J.K. Rowling", eBookUrl: "www.google.com"},
     {title:"Romeo and Juliet", author:"Shakespeare", eBookUrl: "www.yahoo.com"},
-    {title:"The Meaning of Science", author:"Tim Lewens", eBookUrl: "www.apple.com"}];
+    {title:"The Meaning of Science", author:"Tim Lewens", eBookUrl: "www.apple.com"}];*/
   
     return result;
   }
@@ -185,11 +237,12 @@ app.post('/lyrics', async (req, res) => {
         }
     }
 
-    console.log("LYRICS", lyricsText);
+    //console.log("LYRICS", lyricsText);
     //call helper method that processes lyrcis and returns one predefined "theme"
     
     //in the helper method, get ebook data from database and return that data
     let result = await processLyrics(lyricsText);
+    console.log(result);
 
     //we have to eventually send back the ebook data to the frontend
     res.json(result);
